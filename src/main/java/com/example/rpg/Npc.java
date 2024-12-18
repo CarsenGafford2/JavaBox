5fba4e4fc3f79ad1b557f8b0d23bb6da9c0708c5
CarsenGafford2 <carsengafford2@gmail.com>
2024-12-17 17:51:44 -0700

    Reapply "WorkStuff"
    This reverts commit 71619442b1f6f3930f72c752885a1b3cffbd0603.

--- src/main/java/com/example/rpg/Npc.java
+++ src/main/java/com/example/rpg/Npc.java
@@ -1,6 +1,8 @@
 package com.example.rpg;
 
-import java.util.Random;
+import java.util.*;
+
+import javafx.scene.image.Image;
 
 public class Npc {
 
@@ -10,6 +12,8 @@ public class Npc {
     private int maxHeight;
     private int[][] map;
     private String name;
+    private Image img;
+    private String task;
 
     public Npc(int xpos, int ypos, String name, int[][] map) {
         this.xpos = xpos;
@@ -18,38 +22,163 @@ public class Npc {
         this.maxHeight = map.length;
         this.maxWidth = map[0].length;
         this.map = map;
+        this.task = "stone";
+
+        Random rand = new Random();
+        this.img = new Image(getClass().getResource("res/npc/guy" + rand.nextInt(21) + ".png").toString());
     }
 
     public void move() {
-        Random rand = new Random();
-        int temp = rand.nextInt(4);
+        if (this.task.equals("wander")) {
+            Random rand = new Random();
+            int temp = rand.nextInt(4);
+            if (temp == 0) {
+                if (xpos + 1 < maxWidth && map[ypos][xpos + 1] == 0) xpos++;
+            } else if (temp == 1) {
+                if (xpos - 1 >= 0 && map[ypos][xpos - 1] == 0) xpos--;
+            } else if (temp == 2) {
+                if (ypos + 1 < maxHeight && map[ypos + 1][xpos] == 0) ypos++;
+            } else if (temp == 3) {
+                if (ypos - 1 >= 0 && map[ypos - 1][xpos] == 0) ypos--;
+            }
+        } else if (this.task.equals("stone")) {
+            int[] coords = findStone();
+            if (coords != null) {
+                System.out.println("Stone found at: " + coords[1] + ", " + coords[0]);
+                List<Node> path = pathFind(coords);
+                System.out.println(path);
+                if (path != null && path.size() > 1) {
+                    // Get the next step in the path (path.get(1) because path.get(0) is the current position)
+                    Node nextStep = path.get(1);
+    
+                    // Move the NPC step by step using xpos++/ypos++ style updates
+                    if (nextStep.x > xpos) xpos++;
+                    else if (nextStep.x < xpos) xpos--;
+    
+                    if (nextStep.y > ypos) ypos++;
+                    else if (nextStep.y < ypos) ypos--;
+    
+                    System.out.println("Moving to: (" + xpos + ", " + ypos + ")");
+                }
+            }
+        }
+    }    
 
-        if (temp == 0) {
-            if (xpos + 1 < maxWidth) {
-                xpos++;
+    private List<Node> pathFind(int[] destination) {
+        PriorityQueue<Node> openList = new PriorityQueue<>(Comparator.comparingInt(n -> n.fCost));
+        boolean[][] closedList = new boolean[maxHeight][maxWidth];
+    
+        Node start = new Node(xpos, ypos, 0, heuristic(xpos, ypos, destination));
+        openList.add(start);
+    
+        Node[][] nodes = new Node[maxHeight][maxWidth];
+        nodes[ypos][xpos] = start;
+    
+        int[][] directions = {
+            {0, 1}, {1, 0}, {0, -1}, {-1, 0}
+        };
+    
+        while (!openList.isEmpty()) {
+            Node current = openList.poll();
+    
+            System.out.println("Visiting node: (" + current.x + ", " + current.y + ")");
+    
+            if (current.x == destination[0] && current.y == destination[1]) {
+                System.out.println("Destination reached!");
+                return reconstructPath(current);
             }
-        } else if (temp == 1) {
-            if (xpos - 1 >= 0) {
-                xpos--;
+    
+            closedList[current.y][current.x] = true;
+    
+            for (int[] dir : directions) {
+                int newX = current.x + dir[0];
+                int newY = current.y + dir[1];
+    
+                // Check boundaries and obstacles
+                if (newX < 0 || newX >= maxWidth || newY < 0 || newY >= maxHeight) {
+                    continue;
+                }
+    
+                if (map[newY][newX] != 0) {
+                    System.out.println("Skipping obstacle at: (" + newX + ", " + newY + ")");
+                    continue;
+                }
+    
+                if (closedList[newY][newX]) continue;
+    
+                int gCost = current.gCost + 1;
+                int hCost = heuristic(newX, newY, destination);
+                Node neighbor = nodes[newY][newX];
+    
+                if (neighbor == null) {
+                    neighbor = new Node(newX, newY, gCost, hCost, current);
+                    nodes[newY][newX] = neighbor;
+                    openList.add(neighbor);
+                    System.out.println("Adding neighbor to open list: (" + newX + ", " + newY + ")");
+                } else if (gCost < neighbor.gCost) {
+                    neighbor.gCost = gCost;
+                    neighbor.fCost = gCost + hCost;
+                    neighbor.parent = current;
+                    openList.add(neighbor);
+                    System.out.println("Updating neighbor with better G cost: (" + newX + ", " + newY + ")");
+                }
             }
-        } else if (temp == 2) {
-            if (ypos + 1 < maxHeight) {
-                ypos++;
-            }
-        } else if (temp == 3) {
-            if (ypos - 1 >= 0) {
-                ypos--;
-            }
-        }        
+        }
+    
+        System.out.println("No valid path found.");
+        return null;
+    }
+    
+    private int heuristic(int x, int y, int[] destination) {
+        return Math.abs(x - destination[0]) + Math.abs(y - destination[1]);
     }
 
-    public int getxPos() {
-        return this.xpos;
+    private List<Node> reconstructPath(Node endNode) {
+        List<Node> path = new ArrayList<>();
+        Node current = endNode;
+        while (current != null) {
+            path.add(current);
+            current = current.parent;
+        }
+        Collections.reverse(path);
+        return path;
     }
-    public int getyPos() {
-        return this.ypos;
+
+    private int[] findStone() {
+        for (int y = 0; y < maxHeight; y++) {
+            for (int x = 0; x < maxWidth; x++) {
+                if (map[y][x] == 1) {
+                    return new int[]{x, y};
+                }
+            }
+        }
+        return null;
     }
-    public String getName() {
-        return this.name;
+
+    public int getxPos() { return xpos; }
+    public int getyPos() { return ypos; }
+    public String getName() { return name; }
+    public Image getImage() { return img; }
+}
+
+class Node {
+    int x, y;
+    int gCost;
+    int hCost;
+    int fCost;
+    Node parent;
+
+    public Node(int x, int y, int gCost, int hCost) {
+        this.x = x;
+        this.y = y;
+        this.gCost = gCost;
+        this.hCost = hCost;
+        this.fCost = gCost + hCost;
+        this.parent = null;
+    }
+
+    public Node(int x, int y, int gCost, int hCost, Node parent) {
+        this(x, y, gCost, hCost);
+        this.parent = parent;
     }
 }
--- src/main/java/com/example/rpg/PrimaryController.java
+++ src/main/java/com/example/rpg/PrimaryController.java
@@ -7,14 +7,13 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
-
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.GridPane;
 
 public class PrimaryController {
     @FXML
@@ -26,36 +25,20 @@ public class PrimaryController {
 
     private String grass = getClass().getResource("res/grass.png").toString();
     private String rock = getClass().getResource("res/rock.png").toString();
-    private String guy = getClass().getResource("res/npc/guy0.png").toString();
+    private String tree = getClass().getResource("res/tree.png").toString();
 
     private Image grassImg = new Image(grass);
-    private Image guyImg = new Image(guy);
     private Image rockImg = new Image(rock);
+    private Image treeImage = new Image(tree);
 
-    private ArrayList<Npc> npcList = new ArrayList<Npc>();
-    private ArrayList<String> names = new ArrayList<String>();
-
+    private ArrayList<Npc> npcList = new ArrayList<>();
+    private ArrayList<String> names = new ArrayList<>();
 
     public void initialize() {
-        map = new int[][] {
-                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0},
-                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0}
-                
-        };
+        int mapWidth = 500;
+        int mapHeight = 500;
+        map = generateMap(mapWidth, mapHeight);
+
         File file = new File("");
         try {
             file = new File(getClass().getResource("names.txt").toURI());
@@ -65,32 +48,32 @@ public class PrimaryController {
 
         try {
             Scanner scan = new Scanner(file);
-            while(scan.hasNextLine()) {
+            while (scan.hasNextLine()) {
                 names.add(scan.nextLine());
             }
             scan.close();
         } catch (Exception e) {
             System.err.println(e);
         }
-    
+
         gridPane.setFocusTraversable(true);
         gridPane.requestFocus();
 
         Timer t = new Timer();
-        spawnNpc(2, 2);
-        spawnNpc(2, 2);
-        spawnNpc(2, 2);
+
+        spawnNpc(1, 1);
+
         t.schedule(new TimerTask() {
             @Override
             public void run() {
                 for (Npc guy : npcList) {
                     map[guy.getyPos()][guy.getxPos()] = 0;
                     guy.move();
-                    drawNpc(guy); 
+                    drawNpc(guy);
                 }
             }
-        }, 0, 1000);
-    
+        }, 0, 500);
+
         gridPane.sceneProperty().addListener((obs, oldScene, newScene) -> {
             if (newScene != null) {
                 newScene.setOnKeyPressed(event -> {
@@ -103,19 +86,46 @@ public class PrimaryController {
                     } else if (event.getCode() == KeyCode.RIGHT) {
                         x = Math.min(map[0].length - 1, x + 1);
                     }
-                    System.out.println("(" + x + ", " + y + ")");
                     renderMap();
                 });
             }
         });
-    
+
         renderMap();
     }
+
+    private int[][] generateMap(int width, int height) {
+        int[][] generatedMap = new int[height][width];
+        Random random = new Random();
+    
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                long seed = 781029377;
+                double value = OpenSimplex2S.noise2(seed, x, y);
+    
+                if (value < -0.2) {
+                    generatedMap[y][x] = 0; // Stone (1)
+                } else if (value < 0.2) {
+                    if (random.nextDouble() < 0.1) {
+                        generatedMap[y][x] = 3;
+                    } else {
+                        generatedMap[y][x] = 0;
+                    }
+                } else {
+                    generatedMap[y][x] = 0;
+                }
+            }
+        }
+        return generatedMap;
+    }    
 
     private void spawnNpc(int x, int y) {
         Random rand = new Random();
         if (!names.isEmpty()) {
-            npcList.add(new Npc(x, y, names.get(rand.nextInt(names.size())), map));
+            npcList.add(new Npc(y, x, names.get(rand.nextInt(names.size())), map));
+            if (npcList.get(npcList.size()-1).getName().equals("Bodger")) {
+                System.out.println("Bodger Spawned");
+            }
             drawNpc(npcList.get(npcList.size() - 1));
         } else {
             System.err.println("Error: Names list is empty. Cannot spawn NPC.");
@@ -126,37 +136,42 @@ public class PrimaryController {
         map[guy.getyPos()][guy.getxPos()] = 2;
         renderMap();
     }
-    
+
     private void renderMap() {
         Platform.runLater(() -> {
             gridPane.getChildren().clear();
-    
+
             for (int row = 0; row < 10; row++) {
                 for (int col = 0; col < 10; col++) {
                     int mapRow = y + row;
                     int mapCol = x + col;
-    
+
                     if (mapRow >= map.length || mapCol >= map[0].length) {
                         continue;
                     }
-    
-                    Image image;
+
+                    Image image = grassImg;
                     if (map[mapRow][mapCol] == 0) {
                         image = grassImg;
                     } else if (map[mapRow][mapCol] == 1) {
                         image = rockImg;
                     } else if (map[mapRow][mapCol] == 2) {
-                        image = guyImg;
-                    } else {
-                        image = grassImg;
+                        for (Npc guy : npcList) {
+                            if (guy.getxPos() == mapCol && guy.getyPos() == mapRow) {
+                                image = guy.getImage();
+                            }
+                        }
+                    } else if (map[mapRow][mapCol] == 3) {
+                        image = treeImage;
                     }
-    
+
                     ImageView imageView = new ImageView(image);
                     imageView.setFitWidth(50);
                     imageView.setFitHeight(50);
                     imageView.setPreserveRatio(true);
+
                     if (map[mapRow][mapCol] == 2) {
-                        Tooltip t = new Tooltip("Test");
+                        Tooltip t = new Tooltip("Error");
                         for (Npc guy : npcList) {
                             if (guy.getxPos() == mapCol && guy.getyPos() == mapRow) {
                                 t = new Tooltip(guy.getName());
@@ -165,10 +180,18 @@ public class PrimaryController {
                         t.setShowDelay(javafx.util.Duration.ZERO);
                         Tooltip.install(imageView, t);
                     }
-    
+
+                    int clickedRow = mapRow;
+                    int clickedCol = mapCol;
+                    imageView.setOnMouseClicked(event -> {
+                        // spawnNpc(clickedRow, clickedCol);
+                        map[clickedRow][clickedCol] = 1;
+                        System.out.println("Stone placed at: " + clickedRow + ", " + clickedCol);
+                    });
+
                     gridPane.add(imageView, col, row);
                 }
             }
         });
     }
-}    
+}
