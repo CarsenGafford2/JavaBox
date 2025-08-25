1c33bde0c0b7a6ec2a014f9895173be6a4dd92d4
CarsenGafford2 <carsengafford2@gmail.com>
2025-08-25 08:31:18 -0600

    work

---
+++ src/main/java/com/example/rpg/particleEffect.java
@@ -0,0 +1,18 @@
+package com.example.rpg;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class particleEffect {
+
+    private int x;
+    private int y;
+
+    public particleEffect(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void animation(GraphicsContext gc) {
+        
+    }
+}
